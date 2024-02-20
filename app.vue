f031207e2bcfe2af6e619ac2417587f390308eba
miaobuao <miaobuao@outlook.com>
2024-02-20 14:54:07 +0800

    Initial

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import {useNotification} from "naive-ui"
+
+const notify = useNotification()
+
+notify.create({
+  type: "info",
+  content: "baga"
+})
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <NuxtLayout>
+      <n-notification-provider>
+        <n-message-provider>
+          <n-dialog-provider>
+            <n-loading-bar-provider>
+                slot: 
+              <slot></slot>
+            </n-loading-bar-provider>
+          </n-dialog-provider>
+        </n-message-provider>
+      </n-notification-provider>
+    </NuxtLayout>
+  </template>
+  
+  <script setup lang="ts">
+  import {NNotificationProvider, NMessageProvider, NDialogProvider, NLoadingBarProvider} from "naive-ui"
+  </script>
